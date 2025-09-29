f724049c86c26771bf9d31f66b0cb454af341285
Jed Macarilay <jmacarilay@cognitiveai.ph>
2025-09-29 12:51:57 +0800

    remove prod link

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,9 +3,7 @@ import { Link } from "react-router";
 export default function Home() {
   return (
     <div>
-      <Link to="https://gcashapp.page.link/?link=https://gcash.splashscreen/?redirect%3Dgcash%3A%2F%2Fcom.mynt.gcash%2Fapp%2F006300121300%3FappId%3D2170010104980149%2526page%253Dpages%252FwebView%252FwebView&apn=com.globe.gcash.android&isi=520020791&ibi=com.globetel.gcash">Click to Mini Program PROD</Link> <br />
-
-      <Link to="https://gcashdev.page.link/?link=https://gcash.splashscreen/?redirect=gcash%3A%2F%2Fcom.mynt.gcash%2Fapp%2F006300121300%3FappId%3D2170010104980149%2526page%253Dpages%252FwebView%252FwebView&apn=com.globe.gcash.android.uat&ibi=xyz.mynt.gcashdev">Click to Mini Program DEV</Link>
+      <Link to="https://gcashdev.page.link/?link=https://gcash.splashscreen/?redirect=gcash%3A%2F%2Fcom.mynt.gcash%2Fapp%2F006300121300%3FappId%3D2170020246304410%2526page%253Dpages%252FwebView%252FwebView&apn=com.globe.gcash.android.uat&ibi=xyz.mynt.gcashdev">Click to Mini Program DEV</Link>
     </div>
   );
 }
