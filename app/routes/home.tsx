8ce017fab8485eb80692f872da669762bf8ce5cd
Jed Macarilay <jmacarilay@cognitiveai.ph>
2025-09-29 10:25:11 +0800

    update miniprogram url

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 export default function Home() {
   return (
     <div>
-      <Link to="https://gcashdev.page.link/?link=https://gcash.splashscreen/?redirect=gcash://com.mynt.gcash/app/006300121300?appId=2170020233750696&page=pages/index/index&apn=com.globe.gcash.android.uat.tokyo&ibi=xyz.mynt.gcashdev">Click to Mini Program</Link>
+      <Link to="https://gcashdev.page.link/?link=https://gcash.splashscreen/?redirect=gcash%3A%2F%2Fcom.mynt.gcash%2Fapp%2F006300121300%3FappId%3D2170020233750696%2526page%253Dpages%index%252Findex&apn=com.globe.gcash.android.uat.tokyo&ibi=xyz.mynt.gcashdev">Click to Mini Program</Link>
     </div>
   );
 }
