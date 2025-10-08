89d5c98ba7180673713b0cd64eaeeb589becd6f4
Jed Macarilay <jmacarilay@cognitiveai.ph>
2025-10-08 12:50:57 +0800

    update dev link

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 export default function Home() {
   return (
     <div>
-      <Link to="https://gcashdev.page.link/?link=https%3A%2F%2Fgcash.splashscreen%2F%3Fredirect%3Dgcash%3A%2F%2Fcom.mynt.gcash%2Fapp%2F006300121300%3FappId%3D2170020233750696%26page%3Dpages%2Fredirect%2Fredirect%3Fpage%3Dredirect%26code%3Dariel-campaign-2025%3D%26type%3Dreferrals%26apn%3Dcom.globe.gcash.android.uat.tokyo%26ibi%3Dxyz.mynt.gcashdev">Click to Mini Program DEV</Link><br />
+      <Link to="https://gcashdev.page.link/?link=https%3A%2F%2Fgcash.splashscreen%2F%3Fredirect%3Dgcash%3A%2F%2Fcom.mynt.gcash%2Fapp%2F006300121300%3FappId%3D2170020233750696%26page%3Dpages%2Fredirect%2Fredirect%3Fpage%3Dredirect%26code%3Dariel-campaign-2025%3D%26type%3Dreferrals%26apn%3Dcom.globe.gcash.android.uat%26ibi%3Dxyz.mynt.gcashdev">Click to Mini Program DEV</Link><br />
       <Link to="https://gcashapp.page.link/?link=https%3A%2F%2Fgcash.splashscreen%2F%3Fredirect%3Dgcash%3A%2F%2Fcom.mynt.gcash%2Fapp%2F006300121300%3FappId%3D2170020233750696%26page%3Dpages%2Fredirect%2Fredirect%3Fpage%3Dredirect%26code%3Dariel-campaign-2025%3D%26type%3Dreferrals%26apn%3Dcom.globe.gcash.android%26isi%3D520020791%26ibi%3Dcom.globetel.gcash">Click to Mini Program PROD</Link>
     </div>
   );
