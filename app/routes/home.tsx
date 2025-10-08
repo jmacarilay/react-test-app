e2e5fba497c936a274294abecdc479ce95af9886
Jed Macarilay <jmacarilay@cognitiveai.ph>
2025-10-08 14:14:02 +0800

    fix url issue make it 1 paramters

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,30 +1,30 @@
 import { Link } from "react-router";
 
 export default function Home() {
-  const devURL = () => {
-    const queryParams = new URLSearchParams({
-      page: "redirect",
-      code: "ariel-campaign-2025",
-      type: "referrals",
-    }).toString();
+  // const devURL = () => {
+  //   const queryParams = new URLSearchParams({
+  //     page: "redirect",
+  //     code: "ariel-campaign-2025",
+  //     type: "referrals",
+  //   }).toString();
 
-    const path = "pages/redirect/redirect?" + queryParams;
+  //   const path = "pages/redirect/redirect?" + queryParams;
 
-    // parse as URL encoded
-    const encodedPath = encodeURIComponent(path);
+  //   // parse as URL encoded
+  //   const encodedPath = encodeURIComponent(path);
     
-    const baseURL = "https://gcashdev.page.link/?link=";
-    const redirectURL = "https://gcash.splashscreen/?redirect=gcash%3A%2F%2Fcom.mynt.gcash%2Fapp%2F006300121300%3FappId%3D2170020233750696%2526page%253D";
-    const finalParams = "&apn=com.globe.gcash.android.uat&ibi=xyz.mynt.gcashdev";
+  //   const baseURL = "https://gcashdev.page.link/?link=";
+  //   const redirectURL = "https://gcash.splashscreen/?redirect=gcash%3A%2F%2Fcom.mynt.gcash%2Fapp%2F006300121300%3FappId%3D2170020233750696%2526page%253D";
+  //   const finalParams = "&apn=com.globe.gcash.android.uat&ibi=xyz.mynt.gcashdev";
 
-    return baseURL + encodeURIComponent(redirectURL) + encodedPath + finalParams;
-  }
+  //   return baseURL + encodeURIComponent(redirectURL) + encodedPath + finalParams;
+  // }
 
-  console.log("Dev URL:", devURL());
+  // console.log("Dev URL:", devURL());
 
   return (
     <div>
-      <Link to="https://gcashdev.page.link/?link=https%3A%2F%2Fgcash.splashscreen%2F%3Fredirect%3Dgcash%3A%2F%2Fcom.mynt.gcash%2Fapp%2F006300121300%3FappId%3D2170020233750696%2526page%253Dpages%252Fredirect%252Fredirect%253Fpage%253Dredirect%2526code%253Dariel-campaign-2025%2526type%253Dreferrals%2526campaignId%253Dxyz&apn=com.globe.gcash.android.uat&ibi=xyz.mynt.gcashdev">Click to Mini Program DEV</Link><br />
+      <Link to="https://gcashdev.page.link/?link=https%3A%2F%2Fgcash.splashscreen%2F%3Fredirect%3Dgcash%3A%2F%2Fcom.mynt.gcash%2Fapp%2F006300121300%3Fi%3DappId%3D2170020233750696%252Cpage%3Dpages%252Fredirect%252Fredirect%253Fpage%253Dredirect%252Ccode%3Dariel-campaign-2025%252Ctype%3Dreferrals&apn=com.globe.gcash.android.uat&ibi=xyz.mynt.gcashdev">Click to Mini Program DEV</Link><br />
       <Link to="https://gcashdev.page.link/?link=https://gcash.splashscreen/?redirect=gcash%3A%2F%2Fcom.mynt.gcash%2Fapp%2F006300121300%3FappId%3D2170020233750696%2526page%253Dpages%252Fredirect%252Fredirect%253Fcode%253Dabc&apn=com.globe.gcash.android&isi=520020791&ibi=com.globetel.gcash">Click to Mini Program PROD</Link>
     </div>
   );
