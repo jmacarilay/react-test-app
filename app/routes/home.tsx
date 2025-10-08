6c458f06a04e0b1a4bc7cd614fb3cc22b906f289
Jed Macarilay <jmacarilay@cognitiveai.ph>
2025-10-08 14:45:12 +0800

    test again

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -28,7 +28,7 @@ export default function Home() {
 
       <Link to="https://gcashdev.page.link/?link=https%3A%2F%2Fgcash.splashscreen%2F%3Fredirect%3Dgcash%3A%2F%2Fcom.mynt.gcash%2Fapp%2F006300121300%3FappId%3D2170020233750696%2526page%253Dpages%252Fredirect%252Fredirect%253Fparams%253Dreferrals|ariel-campaign-2025&apn=com.globe.gcash.android.uat&ibi=xyz.mynt.gcashdev">Click to Mini Program DEV 1 params only</Link><br />
 
-      <Link to="https://gcashdev.page.link/?link=https://gcash.splashscreen/?redirect=gcash%3A%2F%2Fcom.mynt.gcash%2Fapp%2F006300121300%3FappId%3D2170010104980149%2526page%253Dpages%252Fredirect%252Fredirect%253FselectedProduct%253Dabc%2526query%253DdiscountCode%253Dxyz&apn=com.globe.gcash.android.uat&ibi=xyz.mynt.gcashdev">Gcash sample URL</Link><br />
+      <Link to="https://gcashdev.page.link/?link=https://gcash.splashscreen/?redirect=gcash%3A%2F%2Fcom.mynt.gcash%2Fapp%2F006300121300%3FappId%2170020233750696%2526page%253Dpages%252Fredirect%252Fredirect%253FselectedProduct%253Dabc%2526query%253DdiscountCode%253Dxyz&apn=com.globe.gcash.android.uat&ibi=xyz.mynt.gcashdev">Gcash sample URL</Link><br />
 
       <Link to="https://gcashdev.page.link/?link=https://gcash.splashscreen/?redirect=gcash%3A%2F%2Fcom.mynt.gcash%2Fapp%2F006300121300%3FappId%3D2170020233750696%2526page%253Dpages%252Fredirect%252Fredirect%253Fcode%253Dabc&apn=com.globe.gcash.android&isi=520020791&ibi=com.globetel.gcash">Click to Mini Program PROD</Link>
     </div>
