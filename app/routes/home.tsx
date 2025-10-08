3faad2994c1afded386c6838a945f1b7702ca568
Jed Macarilay <jmacarilay@cognitiveai.ph>
2025-10-08 13:53:26 +0800

    fix params brekable

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -24,7 +24,7 @@ export default function Home() {
 
   return (
     <div>
-      <Link to="https://gcashdev.page.link/?link=https://gcash.splashscreen/?redirect=gcash%3A%2F%2Fcom.mynt.gcash%2Fapp%2F006300121300%3FappId%3D2170020233750696%2526page%253Dpages%252Fredirect%252Fredirect%253Fpage%253Dredirect&code%253Dariel-campaign-2025&type%253Dreferrals&apn=com.globe.gcash.android.uat&ibi=xyz.mynt.gcashdev">Click to Mini Program DEV</Link><br />
+      <Link to="https://gcashdev.page.link/?link=https%3A%2F%2Fgcash.splashscreen%2F%3Fredirect%3Dgcash%3A%2F%2Fcom.mynt.gcash%2Fapp%2F006300121300%3FappId%3D2170020233750696%26page%3Dpages%2Fredirect%2Fredirect%3Fpage%3Dredirect%26code%3Dariel-campaign-2025%26type%3Dreferrals&apn=com.globe.gcash.android.uat&ibi=xyz.mynt.gcashdev">Click to Mini Program DEV</Link><br />
       <Link to="https://gcashdev.page.link/?link=https://gcash.splashscreen/?redirect=gcash%3A%2F%2Fcom.mynt.gcash%2Fapp%2F006300121300%3FappId%3D2170020233750696%2526page%253Dpages%252Fredirect%252Fredirect%253Fcode%253Dabc&apn=com.globe.gcash.android&isi=520020791&ibi=com.globetel.gcash">Click to Mini Program PROD</Link>
     </div>
   );
