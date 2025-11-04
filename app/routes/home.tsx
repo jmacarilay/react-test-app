6d51f26c84c8d3106a6edcaf347402f69699e59a
Jed Macarilay <jmacarilay@cognitiveai.ph>
2025-11-04 16:09:00 +0800

    update color to green

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -237,7 +237,7 @@ export default function Home() {
             <button
               onClick={handleCapture}
               disabled={uploading || !focusGood || !lightGood}
-              className={`flex flex-col gap-1 items-center justify-center font-bold active:scale-95 transition-transform text-lg ${lightGood ? 'text-[#4fc3f7]' : 'text-[#ff9800]'} ${uploading ? 'opacity-60 pointer-events-none' : ''}`}
+              className={`flex flex-col gap-1 items-center justify-center font-bold active:scale-95 transition-transform text-lg ${lightGood ? 'text-[#00ff5a]' : 'text-[#ff9800]'} ${uploading ? 'opacity-60 pointer-events-none' : ''}`}
             >
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-10">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z" />
